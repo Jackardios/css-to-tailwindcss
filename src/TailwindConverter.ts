d7f973e224cc04b7f2309f03981eb9ce0b55a3e5
Salakhutdinov Salavat <salahutdinov.salavat@gmail.com>
2023-01-15 15:00:00 +0300

    update `TailwindConverter`

--- src/TailwindConverter.ts
+++ src/TailwindConverter.ts
@@ -1,17 +1,38 @@
-import type { Selector } from 'css-what';
-import type { AcceptedPlugin, AtRule, Declaration, Rule } from 'postcss';
+import type { AttributeSelector, Selector } from 'css-what';
+import type {
+  AcceptedPlugin,
+  Container,
+  Declaration,
+  Document,
+  Rule,
+} from 'postcss';
 import type { Config } from 'tailwindcss';
 import type { ConverterMapping } from './types/ConverterMapping';
 
-import postcss from 'postcss';
+import postcss, { Comment } from 'postcss';
 import postcssSafeParser from 'postcss-safe-parser';
 import resolveConfig from 'tailwindcss/resolveConfig';
 import { parse, stringify } from 'css-what';
 
-import { TailwindNode, TailwindNodesManager } from './TailwindNodesManager';
+import {
+  isResolvedTailwindNode,
+  isUnresolvedTailwindNode,
+  TailwindNode,
+  TailwindNodesManager,
+} from './TailwindNodesManager';
 import { isAtRuleNode } from './utils/isAtRuleNode';
-import { converterMappingByTailwindTheme } from './utils/converterMappingByTailwindTheme';
-import { TAILWIND_DECLARATION_CONVERTERS } from './constants/converters';
+import {
+  converterMappingByTailwindTheme,
+  normalizeAtRuleParams,
+} from './utils/converterMappingByTailwindTheme';
+import {
+  convertDeclarationValue,
+  TAILWIND_DECLARATION_CONVERTERS,
+} from './constants/converters';
+import { isChildNode } from './utils/isChildNode';
+import { MEDIA_PARAMS_MAPPING } from './constants/media-params-mapping';
+import { removeUnnecessarySpaces } from './utils/removeUnnecessarySpaces';
+import { reduceTailwindClasses } from './utils/reduceTailwindClasses';
 
 export interface TailwindConverterConfig {
   remInPx?: number | null;
@@ -53,52 +74,57 @@ export class TailwindConverter {
     };
   }
 
-  convertCSS(css: string) {
+  async convertCSS(css: string) {
     const nodesManager = new TailwindNodesManager();
+    const parsed = await postcss(this.config.postCSSPlugins).process(css, {
+      parser: postcssSafeParser,
+    });
 
-    postcss(this.config.postCSSPlugins)
-      .process(css, { parser: postcssSafeParser })
-      .then(result => {
-        result.root.walkRules(rule => {
-          nodesManager.mergeNodes(this.convertRule(rule));
-        });
-      });
-
-    return nodesManager.getNodes();
-  }
-
-  convertRule(rule: Rule) {
-    const nodesManager = new TailwindNodesManager();
-
-    rule.selectors.forEach(selector => {
-      let tailwindClasses: string[] = [];
-      let skippedDeclarations: Declaration[] = [];
-
-      rule.walkDecls(declaration => {
-        const converted = this.convertDeclarationToClasses(declaration);
-        if (converted?.length) {
-          tailwindClasses = tailwindClasses.concat(converted);
-        } else {
-          skippedDeclarations.push(declaration);
-        }
-      });
-
-      if (tailwindClasses.length) {
-        nodesManager?.mergeNode(
-          this.makeTailwindNode(
-            tailwindClasses,
-            rule,
-            selector || rule.selector,
-            skippedDeclarations
-          )
-        );
+    parsed.root.walkRules(rule => {
+      const converted = this.convertRule(rule);
+      if (converted) {
+        nodesManager.mergeNode(converted);
       }
     });
 
-    return nodesManager.getNodes();
+    const nodes = nodesManager.getNodes();
+    nodes.forEach(node => {
+      node.rule.prepend(
+        new Comment({
+          text: '@apply ' + node.tailwindClasses.join(' ') + ';\n',
+        })
+      );
+    });
+
+    return {
+      nodes,
+      convertedRoot: parsed.root,
+    };
   }
 
-  convertDeclarationToClasses(declaration: Declaration) {
+  protected convertRule(rule: Rule): TailwindNode | null {
+    let tailwindClasses: string[] = [];
+
+    rule.walkDecls(declaration => {
+      const converted = this.convertDeclarationToClasses(declaration);
+
+      if (converted?.length) {
+        declaration.remove();
+        tailwindClasses = tailwindClasses.concat(converted);
+      }
+    });
+
+    if (tailwindClasses.length) {
+      return this.makeTailwindNode(
+        rule,
+        reduceTailwindClasses(tailwindClasses)
+      );
+    }
+
+    return null;
+  }
+
+  protected convertDeclarationToClasses(declaration: Declaration) {
     return (
       TAILWIND_DECLARATION_CONVERTERS[declaration.prop]?.(
         declaration,
@@ -107,62 +133,85 @@ export class TailwindConverter {
     );
   }
 
-  protected makeTailwindNode(
-    tailwindClasses: string[],
-    rule: Rule,
-    selector: string,
-    skippedDeclarations: Declaration[] = []
-  ): TailwindNode {
-    const parsedSelector = parse(selector)[0];
-    const baseSelectors: Array<Selector> = [];
-    const converted: Array<string> = [];
+  protected makeTailwindNode(rule: Rule, tailwindClasses: string[]) {
+    const parsedSelector = parse(rule.selector);
+    let tailwindNode: TailwindNode = { rule, tailwindClasses };
 
-    parsedSelector?.forEach(item => {
-      const convertedSelector = this.convertSelector(item);
+    if (parsedSelector.length === 1) {
+      const firstParsedSelector = parsedSelector[0];
+      const baseSelectors: Array<Selector> = [];
+      const converted: Array<string> = [];
 
-      if (convertedSelector) {
-        converted.push(convertedSelector);
-      } else if (item.type === 'tag' || item.type === 'attribute') {
-        baseSelectors.push(item);
+      firstParsedSelector?.forEach(item => {
+        const convertedSelector = this.convertSelector(item);
+
+        if (convertedSelector) {
+          converted.push(convertedSelector);
+        } else if (item.type === 'tag' || item.type === 'attribute') {
+          baseSelectors.push(item);
+        }
+      });
+
+      const isComplexSelector =
+        baseSelectors.length + converted.length !== firstParsedSelector.length;
+
+      if (!isComplexSelector) {
+        const classesPrefix = converted.join('');
+
+        tailwindNode = {
+          key: stringify([baseSelectors]),
+          fallbackRule: rule,
+          classesPrefixWhenFound: classesPrefix,
+          tailwindClasses,
+        };
       }
-    });
+    }
 
-    const isComplexSelector =
-      baseSelectors.length + converted.length !== parsedSelector.length;
+    const classPrefix = this.convertContainerToClassPrefix(rule.parent);
 
-    if (!isComplexSelector) {
-      const classesPrefix = converted.join('');
-
-      selector = stringify([baseSelectors]);
-      tailwindClasses = tailwindClasses.map(
-        className => `${classesPrefix}${className}`
+    if (classPrefix) {
+      tailwindNode = isResolvedTailwindNode(tailwindNode)
+        ? {
+            key: tailwindNode.rule.selector,
+            fallbackRule: rule,
+            classesPrefixWhenFound: classPrefix,
+            tailwindClasses,
+          }
+        : {
+            key: tailwindNode.key,
+            fallbackRule: rule,
+            classesPrefixWhenFound:
+              classPrefix + tailwindNode.classesPrefixWhenFound,
+            tailwindClasses,
+          };
+    } else if (isUnresolvedTailwindNode(tailwindNode)) {
+      tailwindNode.key = TailwindNodesManager.convertRuleToKey(
+        rule.clone({ selector: tailwindNode.key })
       );
     }
 
-    // TODO: walk all atRules instead of this:
-    if (isAtRuleNode(rule.parent)) {
-      tailwindClasses = this.prepareClassesByAtRule(
-        tailwindClasses,
-        rule.parent
-      );
-    }
-
-    return {
-      selector,
-      tailwindClasses,
-      skippedDeclarations,
-    };
+    return tailwindNode;
   }
 
   protected convertSelector(selector: Selector) {
     if (selector.type === 'pseudo' || selector.type === 'pseudo-element') {
-      return `${selector.name}:`;
+      return (
+        {
+          'file-selector-button': 'file:',
+          'last-child': 'last:',
+          'only-child': 'only:',
+          'nth-child(odd)': 'odd:',
+          'nth-child(2n+1)': 'odd:',
+          'nth-child(even)': 'even:',
+          'nth-child(2n)': 'even:',
+        }[selector.name] || `${selector.name}:`
+      );
     }
 
     if (selector.type === 'attribute') {
       if (selector.name.startsWith('aria-')) {
-        const mapped =
-          this.config.mapping.aria[stringify([[selector]]).substring(5)];
+        const mappingKey = this.attributeSelectorToMappingKey(selector, 6);
+        const mapped = this.config.mapping.aria?.[mappingKey];
 
         if (!mapped) {
           return null;
@@ -172,8 +221,8 @@ export class TailwindConverter {
       }
 
       if (selector.name.startsWith('data-')) {
-        const mapped =
-          this.config.mapping.data[stringify([[selector]]).substring(5)];
+        const mappingKey = this.attributeSelectorToMappingKey(selector, 6);
+        const mapped = this.config.mapping.data?.[mappingKey];
 
         if (!mapped) {
           return null;
@@ -186,26 +235,112 @@ export class TailwindConverter {
     return null;
   }
 
-  protected prepareClassesByAtRule(
-    tailwindClasses: string[],
-    atRule: AtRule
-  ): string[] {
-    const prefix = this.convertAtRuleToClassPrefix(atRule);
-    tailwindClasses = prefix
-      ? tailwindClasses.map(className => `${prefix}${className}`)
-      : tailwindClasses;
+  protected attributeSelectorToMappingKey(
+    selector: AttributeSelector,
+    from = 1
+  ) {
+    const stringifiedSelector = stringify([[selector]]);
 
-    return isAtRuleNode(atRule.parent)
-      ? this.prepareClassesByAtRule(tailwindClasses, atRule.parent)
-      : tailwindClasses;
+    return stringifiedSelector.substring(from, stringifiedSelector.length - 1);
   }
 
-  protected convertAtRuleToClassPrefix(atRule: AtRule) {
-    // TODO: add implementation
-    if (atRule.name === 'media') {
-      return 'media:';
+  protected convertContainerToClassPrefix(container: Container | undefined) {
+    let currentContainer: Document | Container | undefined = container;
+    const mediaParams: string[] = [];
+    const supportsParams: string[] = [];
+
+    while (isChildNode(currentContainer)) {
+      if (!isAtRuleNode(currentContainer)) {
+        // do not convert if parent is not at-rule
+        return '';
+      }
+
+      if (currentContainer.name === 'media') {
+        mediaParams.push(currentContainer.params);
+      } else if (currentContainer.name === 'supports') {
+        supportsParams.push(currentContainer.params);
+      } else {
+        return '';
+      }
+
+      currentContainer = currentContainer.parent;
     }
 
-    return '';
+    let mediaPrefixes = '',
+      supportsPrefixes = '';
+    if (mediaParams.length) {
+      mediaPrefixes = this.convertMediaParamsToClassPrefix(
+        mediaParams.reverse()
+      );
+      if (!mediaPrefixes) {
+        return;
+      }
+    }
+
+    if (supportsParams.length) {
+      supportsPrefixes = this.convertSupportsParamsToClassPrefix(
+        supportsParams.reverse()
+      );
+      if (!supportsPrefixes) {
+        return;
+      }
+    }
+
+    return mediaPrefixes + supportsPrefixes;
+  }
+
+  protected convertMediaParamsToClassPrefix(mediaParams: string[]) {
+    const modifiers: string[] = [];
+    const screens: string[] = [];
+
+    for (let i = 0; i < mediaParams.length; i++) {
+      const splitted = mediaParams[i].split(' and ');
+      for (let j = 0; j < splitted.length; j++) {
+        const param = normalizeAtRuleParams(splitted[j].trim());
+
+        if (param === 'screen') {
+          continue;
+        }
+
+        if (param.includes('width') || param.includes('height')) {
+          screens.push(param);
+          continue;
+        }
+
+        let mapped = (MEDIA_PARAMS_MAPPING as any)[param.replace(/\s+/g, '')];
+        if (mapped) {
+          modifiers.push(mapped);
+          continue;
+        }
+
+        // do not convert if not convertable media
+        return '';
+      }
+    }
+
+    const mappedScreen = this.config.mapping.screens[screens.join(' and ')];
+
+    if (!mappedScreen) {
+      return '';
+    }
+
+    modifiers.push(mappedScreen);
+
+    const classPrefix = modifiers.join(':');
+
+    return classPrefix ? classPrefix + ':' : '';
+  }
+
+  protected convertSupportsParamsToClassPrefix(supportParams: string[]) {
+    const buildParams = supportParams.join(' and ');
+    const classPrefix = convertDeclarationValue(
+      supportParams.length > 1
+        ? removeUnnecessarySpaces(buildParams)
+        : normalizeAtRuleParams(buildParams),
+      this.config.mapping.supports || {},
+      'supports'
+    );
+
+    return classPrefix ? classPrefix + ':' : '';
   }
 }
