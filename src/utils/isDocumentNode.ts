0694d5a38cfa6e51044e417d776c176fe506e4ea
Salakhutdinov Salavat <salahutdinov.salavat@gmail.com>
2023-01-16 19:25:16 +0300

    remove unnecessary functions

--- src/utils/isDocumentNode.ts
+++
@@ -1,5 +0,0 @@
-import type { Node, Document } from 'postcss';
-
-export function isDocumentNode(node: Node | undefined): node is Document {
-  return !!(node?.type === 'document');
-}
--- src/utils/isRootNode.ts
+++
@@ -1,5 +0,0 @@
-import type { Node, Root } from 'postcss';
-
-export function isRootNode(node: Node | undefined): node is Root {
-  return !!(node?.type === 'root');
-}
